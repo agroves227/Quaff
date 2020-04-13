2b696920ee8100e43c6cb493cc4a9eaa94161421
agroves227 <60018057+agroves227@users.noreply.github.com>
2020-04-13 18:40:22 -0400

    Add files via upload

---
+++ character.gd
@@ -0,0 +1,49 @@
+extends RigidBody2D
+
+# Default Character Properties
+var acceleration = 10000
+var top_move_speed = 200
+var top_jump_speed = 400
+
+
+#Movement Vars
+var directional_force = Vector2()
+const DIRECTION = {
+	ZERO = Vector2(0, 0),
+	LEFT = Vector2(-1, 0),
+	RIGHT = Vector2(1, 0),
+	UP = Vector2(0, -1),
+	DOWN = Vector2(0, 1)
+}
+
+func _integrate_forces(state):
+	#Final Force
+	var final_force = Vector2()
+	
+	# We are not moving when you are not changing the direction
+	directional_force = DIRECTION.ZERO
+	
+	# Apply force on character
+	apply_force(state)
+	
+	# Get movement velocity
+	final_force = state.get_linear_velocity() + (directional_force * acceleration)
+
+	# Prevent ourselves from exceeding top speeds
+	if (final_force.x > top_move_speed):
+		final_force.x = top_move_speed
+	elif(final_force.x < -top_move_speed):
+		final_force.x = -top_move_speed
+		
+	if (final_force.y > top_jump_speed):
+		final_force.y = top_jump_speed
+	elif(final_force.y < -top_jump_speed):
+		final_force.y = -top_jump_speed
+		
+	# Add Force
+	state.set_linear_velocity(final_force)
+
+# This func is overwritten by the character
+func apply_force(state):
+	pass
+	
